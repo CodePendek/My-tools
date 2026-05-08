befd081be773eff935a4eb47f6bd991bcc0a418a
codependek <aliefanarkis@gmail.com>
2026-05-08 13:51:07 +0800

    update banyak

--- sw.js
+++ sw.js
@@ -1,34 +1,30 @@
-const CACHE_NAME = 'tools-hub-v1';
-const BASE_PATH = '/my-tools/';
-
-const urlsToCache = [
-  BASE_PATH,
-  BASE_PATH + 'index.html',
-  BASE_PATH + 'absensi.html',
-  BASE_PATH + 'slip.html',
-  BASE_PATH + 'retase.html',
-  BASE_PATH + 'manifest.json'
-];
+// Dynamic base path - akan diisi otomatis dari scope
+let BASE_PATH = '';
 
 self.addEventListener('install', event => {
+  // Ambil base path dari scope
+  const scopeUrl = new URL(self.registration.scope);
+  BASE_PATH = scopeUrl.pathname;
+  
+  const urlsToCache = [
+    BASE_PATH,
+    BASE_PATH + 'index.html',
+    BASE_PATH + 'absensi.html',
+    BASE_PATH + 'slip.html',
+    BASE_PATH + 'retase.html',
+    BASE_PATH + 'manifest.json'
+  ];
+  
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open('tools-hub-v1')
+    .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-    .then(response => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+    .then(response => response || fetch(event.request))
   );
 });
 
@@ -37,7 +33,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
+          if (cacheName !== 'tools-hub-v1') {
             return caches.delete(cacheName);
           }
         })
