0e02ff3f4f65a0002c8cbbe1b6e78ed8c82da944
codependek <aliefanarkis@gmail.com>
2026-05-08 14:09:24 +0800

    update

--- sw.js
+++ sw.js
@@ -1,37 +1,30 @@
-// Nama cache yang unik untuk proyek ini
-const CACHE_NAME = 'tools-hub-v1';
+// Dynamic base path - akan diisi otomatis dari scope
+let BASE_PATH = '';
 
 self.addEventListener('install', event => {
-  // Ambil base path secara dinamis dari scope pendaftaran
+  // Ambil base path dari scope
   const scopeUrl = new URL(self.registration.scope);
-  const BASE_PATH = scopeUrl.pathname;
+  BASE_PATH = scopeUrl.pathname;
   
   const urlsToCache = [
     BASE_PATH,
     BASE_PATH + 'index.html',
     BASE_PATH + 'absensi.html',
-    BASE_PATH + 'absen.html',
     BASE_PATH + 'slip.html',
     BASE_PATH + 'retase.html',
     BASE_PATH + 'manifest.json'
   ];
   
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => {
-        console.log('Caching app shell...');
-        return cache.addAll(urlsToCache);
-    })
+    caches.open('tools-hub-v1')
+    .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-    .then(response => {
-        // Return dari cache jika ada, jika tidak ambil dari network
-        return response || fetch(event.request);
-    })
+    .then(response => response || fetch(event.request))
   );
 });
 
@@ -40,7 +33,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
+          if (cacheName !== 'tools-hub-v1') {
             return caches.delete(cacheName);
           }
         })
